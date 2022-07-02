9700fb40e1468a133f737564dd2fcb81d95f7030
Edinson Cabrera Ñique <urielkillian2607@hotmail.com>
2022-07-02 00:45:33 -0500

    Fixing user microservices

--- src/main/java/com/webcrew/easystory/usersservice/UsersServiceApplication.java
+++ src/main/java/com/webcrew/easystory/usersservice/UsersServiceApplication.java
@@ -2,12 +2,25 @@ package com.webcrew.easystory.usersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.webcrew.easystory.usersservice.config.AxonConfig;
 
 @SpringBootApplication()
+@Import(AxonConfig.class)
 public class UsersServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder loadBalancedWebBuilder() {
+		return WebClient.builder();
 	}
 
 }
--- src/main/java/com/webcrew/easystory/usersservice/command/api/ClientController.java
+++ src/main/java/com/webcrew/easystory/usersservice/command/api/ClientController.java
@@ -1,18 +1,25 @@
 package com.webcrew.easystory.usersservice.command.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.webcrew.easystory.usersservice.command.application.dtos.ClientView;
 import com.webcrew.easystory.usersservice.command.application.dtos.RegisterClientRequest;
 import com.webcrew.easystory.usersservice.command.application.dtos.RegisterClientResponse;
 import com.webcrew.easystory.usersservice.command.application.notification.Notification;
 import com.webcrew.easystory.usersservice.command.application.notification.Result;
 import com.webcrew.easystory.usersservice.command.application.services.ClientApplicationService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/clients")
+@Tag(name = "Clients")
 public class ClientController {
     private final ClientApplicationService clientApplicationService;
 
@@ -20,6 +27,7 @@ public class ClientController {
         this.clientApplicationService = clientApplicationService;
     }
 
+    @PostMapping("/")
     public ResponseEntity<Object> registerClient(@RequestBody RegisterClientRequest registerClientRequest) {
         try {
             Result<RegisterClientResponse, Notification> result = clientApplicationService
@@ -29,6 +37,17 @@ public class ClientController {
             }
             return ApiController.error(result.getFailure().getErrors());
         } catch (Exception e) {
+            return ApiController.serverError();
+        }
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Object> getByUserId(@PathVariable("id") String id) {
+        try {
+            ClientView clientView = clientApplicationService.getById(id);
+            return ApiController.ok(clientView);
+        } catch (Exception e) {
+            e.printStackTrace();
             return ApiController.serverError();
         }
     }
